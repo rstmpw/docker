a2087e25fa32f1a35b092abb1a98ad00548d4c9d
Advanced <advancedmaxx@gmail.com>
2017-04-30 03:39:58 +0300

    Nginx vagrant provision

---
+++ src/nginx/vagrantprovision.sh
@@ -0,0 +1,2 @@
+bash <(curl -Ls https://raw.githubusercontent.com/rstmpw/docker/master/dist/nginx/build.sh)
+bash <(curl -Ls https://raw.githubusercontent.com/rstmpw/docker/master/dist/nginx/run.sh)
