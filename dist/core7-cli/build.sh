19a257bdc42648929ecd9cd66aba64af3bbf70be
Advanced <advancedmaxx@gmail.com>
2017-05-09 03:23:55 +0300

    pgsql

---
+++ dist/core7-cli/build.sh
@@ -0,0 +1,1 @@
+sudo docker build --no-cache -t rstmpw/core7-cli https://raw.githubusercontent.com/rstmpw/docker/master/dist/core7-cli/Dockerfile
--- dist/php71-cli/build.sh
+++
@@ -1,1 +0,0 @@
-sudo docker build --no-cache -t rstmpw/php71-cli https://raw.githubusercontent.com/rstmpw/docker/master/dist/php71-cli/Dockerfile
