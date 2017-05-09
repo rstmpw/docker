6013f038a4efc8fbb876bc15b9eef1692975237e
Advanced <advancedmaxx@gmail.com>
2017-05-09 03:51:19 +0300

    cli cmd

--- dist/nginx/run.sh
+++ dist/nginx/run.sh
@@ -3,7 +3,6 @@ sudo docker run -d \
     --net docker-rstm-bridge0 \
     -p 80:80 \
     -p 443:443 \
-    -v /vagrant/environment/nginx/dev/vhost.conf:/etc/nginx/conf.d/core7.conf:ro \
     -v /vagrant/app:/srv/core7.rstm.app/app:ro \
     -v /vagrant/appdata/public:/srv/core7.rstm.app/appdata/public:ro \
     -v /vagrant/appdata/private:/srv/core7.rstm.app/appdata/private:ro \
--- dist/php71-fpm/run.sh
+++ dist/php71-fpm/run.sh
@@ -2,7 +2,6 @@ sudo docker run -d \
     --name php-fpm.rstm.app \
     --net docker-rstm-bridge0 \
     -p 9100:9100 \
-    -v /vagrant/environment/php/dev/fpmpool.conf:/usr/local/etc/php-fpm.d/core7.conf:ro \
     -v /vagrant/app:/srv/core7.rstm.app/app:ro \
     -v /vagrant/appdata:/srv/core7.rstm.app/appdata \
     -v /vagrant/appdata/envlog/php-fpm:/var/log/php-fpm \
