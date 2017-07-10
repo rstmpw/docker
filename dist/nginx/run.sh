26fe42208ebde5e7494fa13bd929e1f6271f7981
Advanced <advancedmaxx@gmail.com>
2017-07-10 19:34:41 +0300

    nginx update

--- dist/nginx/run.sh
+++ dist/nginx/run.sh
@@ -3,8 +3,13 @@ sudo docker run -d \
     --net docker-rstm-bridge0 \
     -p 80:80 \
     -p 443:443 \
-    -v /vagrant/app:/srv/core7.rstm.app/app:ro \
-    -v /vagrant/appdata/public:/srv/core7.rstm.app/appdata/public:ro \
-    -v /vagrant/appdata/private:/srv/core7.rstm.app/appdata/private:ro \
-    -v /vagrant/appdata/envlog/nginx:/var/log/nginx \
+    -v /vagrant/app:/vagrant/app:ro \
+    -v /vagrant/appdata/public:/vagrant/appdata/public:ro \
+    -v /vagrant/appdata/private:/vagrant/appdata/private:ro \
+    -v /vagrant/envdata/log/nginx:/var/log/nginx \
     rstmpw/nginx
+
+sudo docker stop nginx.rstm.app
+sudo rm /vagrant/envdata/log/nginx/*
+sudo docker cp /vagrant/environment/nginx/dev/vhost.conf nginx.rstm.app:/etc/nginx/conf.d/core7.conf
+sudo docker start nginx.rstm.app
---
+++ src/nginx/tar.sh
@@ -0,0 +1,1 @@
+tar -cvzf Docker.tar.gz ./Docker
