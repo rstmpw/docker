746df7cccb72d0ad0488ee098422b355a4487409
AdvancedMaXX <advancedmaxx@gmail.com>
2017-10-12 03:33:42 +0300

    create custom network

---
+++ createCustomNet.sh
@@ -0,0 +1,6 @@
+#Define custom docker network
+sudo docker network create \
+        --driver=bridge \
+        --subnet=192.168.183.0/24 \
+        --gateway=192.168.183.1 \
+        custom-network-bridge0
