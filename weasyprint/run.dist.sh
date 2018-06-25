9070178cebdac223afdb7a9c1ec01bb076f6ce1b
AdvancedMaXX <advancedmaxx@gmail.com>
2018-06-25 19:54:03 +0300

    add restart

--- weasyprint/run.dist.sh
+++ weasyprint/run.dist.sh
@@ -1,6 +1,7 @@
 sudo docker run -d \
 	--name weasyweb.local.app \
     -p 4652:4652 \
+    --restart=always \
 `#	-v /css:/opt/weasyweb/resources/css ` \
 `#	-v /log:/opt/weasyweb/log ` \
 `#	-v /etc/localtime:/etc/localtime:ro` \
