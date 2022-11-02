ecb19d7da77a3dedd9bd99e4c98f835880569acc
=MZ= <madzerling@gmail.com>
2022-11-02 13:30:10 +0300

    PHP 8.1.12 IBMMQ

---
+++ php81mqseries/entrypoint-debian.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -e
+
+if [ ! -d /var/spool/cron/crontabs ]; then
+  mkdir -p /var/spool/cron/crontabs
+elif [ "$(ls -A /var/spool/cron/crontabs)" ]; then
+  rm /var/spool/cron/crontabs/*
+fi
+
+if [ "$(ls -A /opt/crontabs)" ]; then
+  cd /opt/crontabs
+  for fl in * ; do
+    uid=$(echo "$fl" | sed -r 's/^u([0-9]+)$/\1/')
+    if [ "$fl" != "$uid" ]; then
+      adduser --disabled-login --disabled-password --gecos "" --uid "$uid" "$fl" || true
+    else
+      adduser --disabled-login --disabled-password --gecos "" "$fl" || true
+    fi
+    crontab -u "$fl" "$fl"
+  done
+fi
+
+syslogd -O /proc/1/fd/1 -S
+
+exec "$@"
