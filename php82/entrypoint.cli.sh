bd54b31adb425c8452a663302a24e7a78ecbe32c
=MZ= <madzerling@gmail.com>
2024-02-15 22:57:04 +0300

    PHP 8.2.15 added PHP_CLI_USER env var to run cli scripts from specified user

--- php82/entrypoint.cli.sh
+++ php82/entrypoint.cli.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 if [ -f /etc/php/php.ini-cli ]; then
-  rm "${PHP_INI_DIR}/conf.d/override.ini"
-  ln -s "/etc/php/php.ini-cli" "${PHP_INI_DIR}/conf.d/override.ini"
+    rm "${PHP_INI_DIR}/conf.d/override.ini"
+    ln -s "/etc/php/php.ini-cli" "${PHP_INI_DIR}/conf.d/override.ini"
 fi
 
 if [ ! -f "$1" ]; then
@@ -10,4 +10,15 @@ if [ ! -f "$1" ]; then
     exit 2
 fi
 
-php -f "$@"
+if [ -z "${PHP_CLI_USER}" ]; then
+    php -f "$@"
+else
+    uid=$(echo "${PHP_CLI_USER}" | sed -r 's/^u([0-9]+)$/\1/')
+    if [ "${PHP_CLI_USER}" != "$uid" ]; then
+        adduser -s /sbin/nologin -D -u "$uid" "${PHP_CLI_USER}" || true
+    else
+        adduser -s /sbin/nologin -D "${PHP_CLI_USER}" || true
+    fi
+
+    runuser -u "${PHP_CLI_USER}" -p -- php -f "$@"
+fi
