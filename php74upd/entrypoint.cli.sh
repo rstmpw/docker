8e5fb445de48ed64fbeb76b14f30888dfac5352a
=MZ= <madzerling@gmail.com>
2024-02-16 11:57:26 +0300

    PHP 7.4.33 added PHP_CLI_USER env var to run cli scripts from specified user

--- php74upd/entrypoint.cli.sh
+++ php74upd/entrypoint.cli.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 if [ -f /etc/php/php.ini-cli ]; then
-  rm "${PHP_INI_DIR}/conf.d/override.ini"
-  ln -s "/etc/php/php.ini-cli" "${PHP_INI_DIR}/conf.d/override.ini"
+    rm "${PHP_INI_DIR}/conf.d/override.ini"
+    ln -s "/etc/php/php.ini-cli" "${PHP_INI_DIR}/conf.d/override.ini"
 fi
 
 if [ ! -f "$1" ]; then
@@ -10,4 +10,15 @@ if [ ! -f "$1" ]; then
     exit 2
 fi
 
-php -f "$@"
+if [ -z "${PHP_CLI_USER}" ]; then
+    php -f "$@"
+else
+    uid=$(echo "$fl" | sed -r 's/^u([0-9]+)$/\1/')
+    if [ "$fl" != "$uid" ]; then
+        adduser --disabled-login --disabled-password --gecos "" --uid "$uid" "$fl" || true
+    else
+        adduser --disabled-login --disabled-password --gecos "" "$fl" || true
+    fi
+
+    runuser -u "${PHP_CLI_USER}" -p -- php -f "$@"
+fi
